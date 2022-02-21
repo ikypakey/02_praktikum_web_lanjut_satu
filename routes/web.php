56c3855189e3ef150284adbd39d93a7853116199
masbarok193@gmail.com <masbarok193@gmail.com>
2022-02-21 10:02:03 +0700

    Perubahan Praktikum 1

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('Selamat Datang');
+});*/
+
+Route::get('/', function () {
+     return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+     return 'Muhammad Rizki Mubarok 2041720001' ; 
+     
+});
+
+Route::get('/articles/{id}', function ($id) {
+     return 'Halaman Artikel dengan ID '. $id ; 
+     
+});
